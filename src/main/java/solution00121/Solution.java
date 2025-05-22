49098032ec1fa2eaf057ddff5404e935d6f6abc8
Viktor Csomor <viq@cloudera.com>
2025-05-22 16:15:26 +0200

    Leetcode 121: Best Time to Buy and Sell Stok

---
+++ src/main/java/solution00121/Solution.java
@@ -0,0 +1,18 @@
+package solution00121;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i: prices) {
+            if (i < minPrice) {
+                minPrice = i;
+            } else if ((i - minPrice) > maxProfit) {
+                maxProfit = i - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+}
---
+++ src/test/java/solution00121/TestSolution.java
@@ -0,0 +1,49 @@
+package solution00121;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSolution {
+    static class Input {
+        int[] nums;
+
+        Input(int[] nums) {
+            this.nums = nums;
+        }
+
+        static Input of(int[] nums) {
+            return new Input(nums);
+        }
+    }
+
+    static class Expected {
+        int n;
+
+        Expected(int n) {
+            this.n = n;
+        }
+
+        static Expected of(int n) {
+            return new Expected(n);
+        }
+    }
+
+    private static Stream<Arguments> casesForSolution() {
+        return Stream.of(
+                Arguments.of(Input.of(new int[]{7,1,5,3,6,4}), Expected.of(5)),
+                Arguments.of(Input.of(new int[]{7,6,4,3,1}), Expected.of(0))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("casesForSolution")
+    void testSolutionCases(Input input, Expected expected) {
+        var n = new Solution().maxProfit(input.nums);
+        assertEquals(expected.n, n);
+    }
+}
